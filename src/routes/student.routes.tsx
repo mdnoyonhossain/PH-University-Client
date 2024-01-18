b5fab2a498757e049a205d6408ff370a7b8538de
Noyon Hossain <mdshahinurrahmannayon@gmal.com>
2024-01-18 05:27:07 -0800

    PH-University Add Authentication

---
+++ src/routes/student.routes.tsx
@@ -0,0 +1,12 @@
+export const studentPaths = [
+    {
+        name: 'Dashboard',
+        path: 'dashboard',
+        element: 'STUDENT_DASHBORD'
+    },
+    {
+        name: 'Offered Course',
+        path: 'offered-course',
+        element: 'OFFERED_COURSE'
+    },
+]
